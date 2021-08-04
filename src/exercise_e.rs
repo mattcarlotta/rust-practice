08e5d0cb0bec102e359c20de89b5d109adffdd44
Matt <carlotta.matt@gmail.com>
2021-08-04 16:20:44 -0700

    Refactor exercise E

--- src/exercise_e.rs
+++ src/exercise_e.rs
@@ -1,5 +1,5 @@
 // Silence some warnings so they don't distract from the exercise.
-#![allow(unused_mut)]
+// #![allow(unused_mut)]
 
 pub fn run() {
   // This fancy stuff either gets the first argument as a String, or prints
